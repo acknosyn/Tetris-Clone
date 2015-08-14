afce1533118d2952cab3175d6fc1951ec2c27b60
Francesco Badraun <fabadraun@gmail.com>
2015-08-14 21:49:53 +1200

    Add 'tick' timing logic

--- Tetris Clone/GameScene.swift
+++ Tetris Clone/GameScene.swift
@@ -8,11 +8,13 @@
 
 import SpriteKit
 
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+    
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
     
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supported")
@@ -27,5 +29,31 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        
+        // if lastTick is missing, game is in paused state
+        if lastTick == nil {
+            return
+        }
+        
+        // recover time passed since last update
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        
+        // if enough time has elapsed, report a tick
+        if timePassed > tickLengthMillis {
+            lastTick = NSDate()
+            tick?()
+        }
+    }
+    
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
 }
