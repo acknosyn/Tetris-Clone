ece0dede0c9d0d6fc15210f345f50729f694ff4d
Francesco Badraun <fabadraun@gmail.com>
2015-08-14 21:31:19 +1200

    Add Array2D class

---
+++ Tetris Clone/Array2D.swift
@@ -0,0 +1,33 @@
+//
+//  Array2D.swift
+//  Tetris Clone
+//
+//  Created by Francesco Badraun on 14/08/15.
+//  Copyright (c) 2015 Pixel Sharp. All rights reserved.
+//
+
+// generics OP
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count: rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * column) + column]
+        }
+        
+        set {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
